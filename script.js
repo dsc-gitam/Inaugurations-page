e79fda880b2636d59c26128dee78be165d0e2783
Janvit __ jp <jyothiprakashpatnaik@gmail.com>
2022-06-08 23:35:47 +0530

    Push Curtain-raiser v0.5

---
+++ script.js
@@ -0,0 +1,12 @@
+var oB = document.getElementsByClassName("openButton");
+var oC = document.getElementsByClassName("onCurtain");
+oB.addEventListner('click',()=>{
+  ob.style.display='none'
+    var x = document.getElementsByClassName("onCurtain");
+    x.style.display = "none";
+    console.log('hello');
+})
+
+document.getElementById("x2000").addEventListener("click",function(){
+  console.log("a");
+})
